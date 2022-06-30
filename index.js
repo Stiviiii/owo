5698b8cf5546a593c7524ff39669b78c9ca62f21
Stivi <104741559+Stiviiii@users.noreply.github.com>
2022-06-30 12:25:12 -0300

    Commit iniciar

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
+say('Stivi - Bot\nWhatsApp Bot MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Stivi Divi nigi pigi mainigi`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
